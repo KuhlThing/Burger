97a69aa53047f76ce7c547551bb7a9dd4e382115
Patrick McDowell <kuhlthing@gmail.com>
2020-02-15 11:38:49 -0500

    wrote most files

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,36 @@
+$(function() {
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevouredBurger = {
+      devoured: true
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredBurger
+    }).then(
+      function() {
+        console.log("changed devoured to", newDevouredBurger);
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+});
