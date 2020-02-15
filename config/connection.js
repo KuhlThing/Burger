0fe9e52e09e2a0d64f0bc53a9babc3f1d052dc60
Patrick McDowell <kuhlthing@gmail.com>
2020-02-15 16:08:55 -0500

    attempting to fix

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,8 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+}
+else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,7 +14,7 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
